0dfccbaa7fc40e64efc692498bc2b170769f9dfe
Alan Cho <alancho@Alans-MBP.fios-router.home>
2021-08-24 15:17:46 -0400

    added sign up form to header module

--- client/src/components/Modules/Header/Header.js
+++ client/src/components/Modules/Header/Header.js
@@ -22,6 +22,7 @@ const Header = () => {
   const classes = useStyles();
 
   const [state, setState] = useState(false);
+  const [toggleSignInUp, setToggleSignInUp] = useState(false);
 
   const toggleDrawer = (open) => (event) => {
     if (
@@ -32,6 +33,10 @@ const Header = () => {
     }
 
     setState(open);
+  };
+
+  const signInUpHandler = () => {
+    setToggleSignInUp(!toggleSignInUp);
   };
 
   return (
@@ -52,60 +57,114 @@ const Header = () => {
               <div role='presentation'>
                 <Container component='main' maxWidth='xs'>
                   <CssBaseline />
-                  <div className={classes.paper}>
-                    <Typography component='h1' variant='h5'>
-                      Sign in
-                    </Typography>
-                    <form className={classes.form} noValidate>
-                      <TextField
-                        variant='outlined'
-                        margin='normal'
-                        required
-                        fullWidth
-                        id='email'
-                        label='Email Address'
-                        name='email'
-                        autoComplete='email'
-                        autoFocus
-                      />
-                      <TextField
-                        variant='outlined'
-                        margin='normal'
-                        required
-                        fullWidth
-                        name='password'
-                        label='Password'
-                        type='password'
-                        id='password'
-                        autoComplete='current-password'
-                      />
-                      <FormControlLabel
-                        control={<Checkbox value='remember' color='primary' />}
-                        label='Remember me'
-                      />
-                      <Button
-                        type='submit'
-                        fullWidth
-                        variant='contained'
-                        color='primary'
-                        className={classes.submit}
-                      >
-                        Sign In
-                      </Button>
-                      <Grid container>
-                        <Grid item xs>
-                          <Link href='#' variant='body2'>
-                            Forgot password?
-                          </Link>
+                  {!toggleSignInUp ? (
+                    <div className={classes.paper}>
+                      <Typography component='h1' variant='h5'>
+                        Sign in
+                      </Typography>
+                      <form className={classes.form} noValidate>
+                        <TextField
+                          variant='outlined'
+                          margin='normal'
+                          required
+                          fullWidth
+                          id='email'
+                          label='Email Address'
+                          name='email'
+                          autoComplete='email'
+                          autoFocus
+                        />
+                        <TextField
+                          variant='outlined'
+                          margin='normal'
+                          required
+                          fullWidth
+                          name='password'
+                          label='Password'
+                          type='password'
+                          id='password'
+                          autoComplete='current-password'
+                        />
+                        <FormControlLabel
+                          control={
+                            <Checkbox value='remember' color='primary' />
+                          }
+                          label='Remember me'
+                        />
+                        <Button
+                          type='submit'
+                          fullWidth
+                          variant='contained'
+                          color='primary'
+                          className={classes.submit}
+                        >
+                          Sign In
+                        </Button>
+                        <Grid container>
+                          <Grid item style={{ margin: "auto" }}>
+                            <Link onClick={signInUpHandler} variant='body2'>
+                              {"Don't have an account? Sign Up"}
+                            </Link>
+                          </Grid>
                         </Grid>
-                        <Grid item>
-                          <Link href='#' variant='body2'>
-                            {"Don't have an account? Sign Up"}
-                          </Link>
+                      </form>
+                    </div>
+                  ) : (
+                    <div className={classes.paper}>
+                      <Typography component='h1' variant='h5'>
+                        Sign Up
+                      </Typography>
+                      <form className={classes.form} noValidate>
+                        <TextField
+                          variant='outlined'
+                          margin='normal'
+                          required
+                          fullWidth
+                          id='email'
+                          label='Email Address'
+                          name='email'
+                          autoComplete='email'
+                          autoFocus
+                        />
+                        <TextField
+                          variant='outlined'
+                          margin='normal'
+                          required
+                          fullWidth
+                          name='password'
+                          label='Password'
+                          type='password'
+                          id='password'
+                        />
+                        <TextField
+                          variant='outlined'
+                          margin='normal'
+                          required
+                          fullWidth
+                          name='confirm-password'
+                          label='Confirm Password'
+                          type='confirm-password'
+                          id='confirm-password'
+                        />
+                        <Button
+                          type='submit'
+                          fullWidth
+                          variant='contained'
+                          color='primary'
+                          className={classes.submit}
+                        >
+                          Sign Up
+                        </Button>
+                        <Grid container>
+                          <Grid item style={{ margin: "auto" }}>
+                            <Link onClick={signInUpHandler} variant='body2'>
+                              {"Already have an account? Sign In"}
+                            </Link>
+                          </Grid>
                         </Grid>
-                      </Grid>
-                    </form>
-                  </div>
+                      </form>
+                    </div>
+                  )}
                 </Container>
               </div>
             </Drawer>
