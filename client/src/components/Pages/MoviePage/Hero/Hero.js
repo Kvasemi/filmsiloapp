acc8a2e317fd79fea5b3442f390f91cc42602e66
Alan Cho <alancho@Alans-MBP.fios-router.home>
2021-09-24 01:40:35 -0400

    converted hero moviepage to grid layout

--- client/src/components/Pages/MoviePage/Hero/Hero.js
+++ client/src/components/Pages/MoviePage/Hero/Hero.js
@@ -1,4 +1,4 @@
-import { CardMedia, Container, Typography } from "@material-ui/core";
+import { CardMedia, Grid, Typography } from "@material-ui/core";
 
 import useStyles from "./styles";
 import { useAuth } from "../../../../context/AuthContext";
@@ -26,11 +26,6 @@ const Hero = () => {
           crew && (
             <Typography
               key={crew.id}
-              style={{
-                display: "inline-block",
-                marginRight: "20px",
-                marginBottom: "10px",
-              }}
               className={classes.text_names}
               variant='body1'
               onClick={() => personClickHandler(crew.id)}
@@ -43,12 +38,12 @@ const Hero = () => {
   };
 
   return (
-    <Container maxWidth={false} className={classes.container}>
-      <CardMedia
-        className={classes.backgroundCard}
-        image={`https://image.tmdb.org/t/p/w780${movie.poster_path}`}
-      >
-        <div className={classes.gridContainer}>
+    <Grid container spacing={4} className={classes.container}>
+      <Grid item xs={12} lg={4} className={classes.gridContainer}>
+        <CardMedia
+          className={classes.backgroundCard}
+          // image={`https://image.tmdb.org/t/p/w780${movie.poster_path}`}
+        >
           <img
             className={classes.image}
             src={
@@ -58,67 +53,69 @@ const Hero = () => {
             }
             alt={movie.title}
           />
-          <div className={classes.textDiv}>
-            <Typography className={classes.movieDetails} variant='h2'>
-              <strong>{movie.title}</strong>
-            </Typography>
-            <Typography
-              className={classes.movieDetails}
-              variant='body1'
-              gutterBottom
-            >
-              {movie.release_date && formatDate(movie.release_date)}
-            </Typography>
-            <Typography
-              className={classes.movieDetails}
-              variant='h6'
-              style={{ marginTop: "50px" }}
-            >
-              <strong>Overview</strong>
-            </Typography>
-            <Typography className={classes.movieDetails} variant='body1'>
-              {movie.overview}
-            </Typography>
-            {mappedCrewList("Directing", "Director").length > 0 && (
-              <div className={classes.text_container}>
-                <Typography
-                  className={classes.text}
-                  variant='subtitle2'
-                  style={{ marginTop: "50px" }}
-                >
-                  Director
-                </Typography>
-                {mappedCrewList("Directing", "Director")}
-              </div>
-            )}
-            {mappedCrewList("Writing", "Screenplay").length > 0 && (
-              <div className={classes.text_container}>
-                <Typography
-                  className={classes.text}
-                  variant='subtitle2'
-                  style={{ marginTop: "10px" }}
-                >
-                  Screenwriter
-                </Typography>
-                {mappedCrewList("Writing", "Screenplay")}
-              </div>
-            )}
-            {mappedCrewList("Production", "Producer").length > 0 && (
-              <div className={classes.text_container}>
-                <Typography
-                  className={classes.text}
-                  variant='subtitle2'
-                  style={{ marginTop: "20px" }}
-                >
-                  Producer
-                </Typography>
-                {mappedCrewList("Production", "Producer")}
-              </div>
-            )}
-          </div>
+        </CardMedia>
+      </Grid>
+      <Grid item xs={12} lg={8} className={classes.gridContainer}>
+        <div className={classes.textDiv}>
+          <Typography className={classes.movieDetails} variant='h2'>
+            <strong>{movie.title}</strong>
+          </Typography>
+          <Typography
+            className={classes.movieDetails}
+            variant='body1'
+            gutterBottom
+          >
+            {movie.release_date && formatDate(movie.release_date)}
+          </Typography>
+          <Typography
+            className={classes.movieDetails}
+            variant='h6'
+            style={{ marginTop: "50px" }}
+          >
+            <strong>Overview</strong>
+          </Typography>
+          <Typography className={classes.movieDetails} variant='body1'>
+            {movie.overview}
+          </Typography>
+          {mappedCrewList("Directing", "Director").length > 0 && (
+            <div className={classes.text_container}>
+              <Typography
+                className={classes.text}
+                variant='subtitle2'
+                style={{ marginTop: "50px" }}
+              >
+                Director
+              </Typography>
+              {mappedCrewList("Directing", "Director")}
+            </div>
+          )}
+          {mappedCrewList("Writing", "Screenplay").length > 0 && (
+            <div className={classes.text_container}>
+              <Typography
+                className={classes.text}
+                variant='subtitle2'
+                style={{ marginTop: "10px" }}
+              >
+                Screenwriter
+              </Typography>
+              {mappedCrewList("Writing", "Screenplay")}
+            </div>
+          )}
+          {mappedCrewList("Production", "Producer").length > 0 && (
+            <div className={classes.text_container}>
+              <Typography
+                className={classes.text}
+                variant='subtitle2'
+                style={{ marginTop: "20px" }}
+              >
+                Producer
+              </Typography>
+              {mappedCrewList("Production", "Producer")}
+            </div>
+          )}
         </div>
-      </CardMedia>
-    </Container>
+      </Grid>
+    </Grid>
   );
 };
 
--- client/src/components/Pages/MoviePage/Hero/styles.js
+++ client/src/components/Pages/MoviePage/Hero/styles.js
@@ -15,6 +15,10 @@ export default makeStyles(() => ({
       position: "fixed",
     },
     webkitTransform: "translate3d(0,0,0)",
+    // "@media (max-width: 412px)": {
+    //   left: "60px",
+    //   width: "100vw",
+    // },
   },
   gridContainer: {
     background: "rgba(0,0,0,0.8)",
@@ -29,13 +33,26 @@ export default makeStyles(() => ({
     height: "450px",
     borderRadius: "10px",
     boxShadow: "10px 5px 5px rgba(0, 0, 0, .2)",
+    "@media (max-width: 376px)": {
+      margin: "auto 20px 0 20px",
+    },
   },
-  cardMedia: { paddingTop: "56.25%", height: "400px" },
+  // cardMedia: { paddingTop: "56.25%", height: "400px" },
   textDiv: {
     margin: "50px 100px 0px 50px",
+    "@media (max-width: 376px)": {
+      margin: "auto 20px 0 20px",
+    },
   },
   movieDetails: { color: "white" },
-  text_container: { display: "inline-block", marginRight: "200px" },
+  text_container: {
+    display: "inline-block",
+    marginRight: "200px",
+    "@media (max-width: 376px)": {
+      margin: "auto",
+      width: "200px",
+    },
+  },
   text: {
     color: "white",
   },
@@ -44,6 +61,15 @@ export default makeStyles(() => ({
     "&:hover": {
       color: "rgba(223, 234, 255, 0.9)",
     },
+    display: "inline-block",
+    marginRight: "20px",
+    marginBottom: "10px",
     cursor: "pointer",
+    "@media (max-width: 376px)": {
+      display: "inline-block",
+      width: "110px",
+      marginRight: "0px",
+      marginBottom: "10px",
+    },
   },
 }));
